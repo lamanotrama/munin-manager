dc9240f5362e6757022708e97dc7a01df96cc17b
lamanotrama <lamanotrama@gmail.com>
2012-10-22 21:24:22 +0900

    見難いのでpod形式で書きなおした。

--- bin/confgen.pl
+++ bin/confgen.pl
@@ -1,13 +1,29 @@
+
+# がっつり社内システムに依存してるので秘密で…
+
+
+=head1 NAME
+
+ munin-manager
+
+=head1 SYNOPSIS
+
+  */10 * * * *  root  /usr/local/munin-manager/bin/confgen.pl
+
+対象ホストが増減した時だけ叩くようにするのもアリ。
+
+=head1 DESCRIPTION
+
 muninのconfigジェネレータスクリプト
 
-がっつり社内システムに依存してるので秘密で…
+各クラスタノード用のconfigはこの下にホスト名をファイル名にして作成
 
- * 各クラスタノード用のconfigはこの下にホスト名をファイル名にして作成
+    /usr/local/munin-manager/var/conf.d/
 
-   /usr/local/munin-manager/var/conf.d/
+munin-manger用のconfigはこここに
 
- * munin-manger用のconfigはこここに
-
-   /etc/munin/munin-manager.conf
+    /etc/munin/munin-manager.conf
 
 configのテンプレート(Text::Xslate用)はtemplates以下にあるので参考にしてください。
+
+=cut
